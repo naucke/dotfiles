a59ebe64405e677fd860e3a4cbb5e32b2b8c7807
Jakob Naucke <jakob.naucke@posteo.de>
2022-11-23 18:35:16 +0100

    Emacs don't save passwords

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -5,6 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(package-selected-packages
    '(; Languages
      auctex elpy flymake-shellcheck go-mode lean-mode rust-mode yaml-mode
