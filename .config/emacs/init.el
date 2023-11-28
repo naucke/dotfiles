e0504f1fbed657026f84654f6478d9471f956b85
Jakob Naucke <jakob.naucke@posteo.de>
2023-11-28 22:21:37 +0100

    remove --erasure from agda flags
    should go in agda2hs source file instead

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -6,7 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(agda2-program-args '("--disable-backend" "--erasure"))
+ '(agda2-program-args '("--disable-backend"))
  '(auth-source-save-behavior nil)
  '(custom-safe-themes
    '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961"))
