e556420cafa46c5c033cd6995f4a8292fdc8b14b
Jakob Naucke <jakob.naucke@posteo.de>
2023-12-29 12:14:59 +0100

    agda/agda2hs updates

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -6,7 +6,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(agda2-program-args '("--disable-backend"))
  '(auth-source-save-behavior nil)
  '(custom-safe-themes
    '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961"))
@@ -177,8 +176,7 @@
 (add-hook 'lean-mode-hook (lambda () (set-input-method 'Lean)))
 (add-hook 'python-mode-hook 'elpy-enable)
 (load-file (let ((coding-system-for-read 'utf-8))
-                (shell-command-to-string "agda2hs-mode locate")))
-;                 (shell-command-to-string "agda-mode locate")))
+                 (shell-command-to-string "agda-mode locate")))
 
 ; Whitespace
 (add-hook 'rust-mode-hook (lambda () (setq indent-tabs-mode nil)))
